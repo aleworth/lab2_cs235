3b7daeea3ac49d36b1032bc7e775b09990bb2fff
aleworth <austinaylworth@gmail.com>
2017-09-27 17:28:40 -0600

    v4

--- LinkedList.h
+++ LinkedList.h
@@ -7,16 +7,72 @@ using namespace std;
 
 template<typename T>
 
-class LinkedList : public LinkedListInterface (void)
+class LinkedList : public LinkedListInterface ()
 {
   public:
     LinkedList();
     ~LinkedList();
 
-    void insertHead(T value);
-    void insertTail(T value);
-    void insertAfter(T value, T insertionNode);
-    void remove(T value);
+    void insertHead(T) {
+      if (head == NULL) {
+        head = new Node(T);
+        head->next = NULL;
+
+      }
+      else {
+        Node* temp = new Node(T);
+        temp->next = head;
+        head = temp;
+        delete temp;
+      }
+    }
+
+    void insertTail(T) {
+      if (head == NULL) {
+        insertHead(T);
+      }
+      else if (head != NULL) {
+        Node* new_pnt = new Node(T);
+        new_pnt->next = NULL;
+        Node* temp = head;
+        bool test = true;
+        while (test) {
+          if (temp->next == NULL) {
+            temp->next = new_pnt;
+            delete temp;
+            test = false;
+        }
+        temp = temp->next;
+      }
+      }
+    }
+
+    void insertAfter(T) {
+
+    }
+
+    void remove(T) {
+      if (T = head) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+      }
+      else if (T->next = NULL) {
+
+      }
+      else {
+        Node* temp2 = head;
+        while (temp2->next->data != T) {
+            temp2 = temp2->next;
+            if (temp2->next == NULL && temp2->next->data != T) {
+              break;
+            }
+        }
+        Node* temp3 = temp2->next;
+        temp2 = temp3->next;
+        delete temp3;
+      }
+    }
     void clear();
     T at(int index);
     int size();
@@ -24,10 +80,9 @@ class LinkedList : public LinkedListInterface (void)
 
   private:
       struct Node {
-        T value data;
+        T data;
         Node* next;
-
-        Node(const T value& data_item, Node* next_ptr = Null) : data(data_item), next(next_ptr) {}
+        Node(const T& data_item, Node* next_ptr = Null) : data(data_item), next(next_ptr) {}
       }
       Node* head = NULL;
 
