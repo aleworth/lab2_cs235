75aedb2baa40ee65a94b6e90ec125951fc32991c
aleworth <austinaylworth@gmail.com>
2017-10-06 10:54:12 -0600

    Add files via upload

--- LinkedList.h
+++ LinkedList.h
@@ -2,6 +2,8 @@
 #define L_L_
 
 #include <stdexcept>
+#include <sstream>
+#include <cmath>
 #include "LinkedListInterface.h"
 
 using namespace std;
@@ -11,40 +13,22 @@ template<typename T>
 class LinkedList : public LinkedListInterface<T>
 {
   public:
-    LinkedList();
-    ~LinkedList();
+    LinkedList() {};
+    ~LinkedList() {};
 
-	bool find(T) {
-		Node* temp = head;
-		if (temp == NULL) {
-			delete temp;
-			return false;
-		}
-		while (temp != T) {
-			temp = temp->next;
-			if (temp == T) {
-				delete temp;
-				return true;
-			}
-			else if (temp == NULL) {
-				delete temp;
-				return false;
-			}
-		}
-	}
 
-	void insertHead(T) {
+	void insertHead(T value) {
 
-		bool check = LinkedList::find(T);
+		bool check = find(value);
 		if (check == true) {
 
 			if (head == NULL) {
-				head = new Node(T);
+				head = new Node(value);
 				head->next = NULL;
 
 			}
 			else {
-				Node* temp = new Node(T);
+				Node* temp = new Node(value);
 				temp->next = head;
 				head = temp;
 				delete temp;
@@ -53,17 +37,17 @@ class LinkedList : public LinkedListInterface<T>
 		else {
 			cout << "T is already in the current Linked List." << endl;
 		}
-    }
+  };
 
-	void insertTail(T) {
+	void insertTail(T value) {
 
-		bool check = LinkedList::find(T);
+		bool check = find(value);
 		if (check == false) {
 			if (head == NULL) {
-				insertHead(T);
+				insertHead(value);
 			}
 			else if (head != NULL) {
-				Node* gnu_pnt = new Node(T);
+				Node* gnu_pnt = new Node(value);
 				gnu_pnt->next = NULL;
 				Node* temp = head;
 				bool test = true;
@@ -80,14 +64,14 @@ class LinkedList : public LinkedListInterface<T>
 		else {
 			cout << "T is already in the current Linked List." << endl;
 		}
-	}
+	};
 
     void insertAfter(T value, T insertionNode) {
-		bool check = LinkedList::find(value);
-		bool recheck = LinkedList::find(insertionNode);
+		bool check = find(value);
+		bool recheck = find(insertionNode);
 		if (recheck == true && check == false) {
 			Node* temp = head;
-			while (temp != insertionNode) {
+			while (temp->data != insertionNode) {
 				temp = temp->next;
 			}
 			Node* gnu_pnt2 = new Node(value);
@@ -101,24 +85,21 @@ class LinkedList : public LinkedListInterface<T>
 			cout << "T is already in the current list." << endl;
 		}
 
-    }
+  };
 
-    void remove(T) {
-		bool check = LinkedList::find(T);
+    void remove(T value) {
+		bool check = find(value);
 		if (check == true) {
-			if (T = head) {
+			if (value == head->data) {
 				Node* temp = head;
 				head = head->next;
 				delete temp;
 			}
-			else if (T->next = NULL) {
-
-			}
 			else {
 				Node* temp2 = head;
-				while (temp2->next->data != T) {
+				while (temp2->next->data != value) {
 					temp2 = temp2->next;
-					if ((temp2->next == NULL) && (temp2->next->data != T)) {
+					if ((temp2->next == NULL) && (temp2->next->data != value)) {
 						break;
 					}
 				}
@@ -128,20 +109,20 @@ class LinkedList : public LinkedListInterface<T>
 			}
 		}
 		else {
-			cout << "T is not in the current Linked List." << endl;
+			cout << "Value is not in the current Linked List." << endl;
 		}
-    }
+  };
 
 	void clear() {
 		while (head != NULL) {
-			LinkedList::remove(head);
+			remove(head->data);
 		}
-	}
+	};
 
 	T at(int index) {
-		int check = LinkedList::size();
+		int check = size();
 		if (check < index) {
-			throw out_of_range;
+			throw out_of_range ("out_of_range");
 		}
 		else {
 			int i = 0;
@@ -152,7 +133,7 @@ class LinkedList : public LinkedListInterface<T>
 			}
 			return temp5->data;
 		}
-	}
+	};
 
 	int size() {
 		int i = 0;
@@ -166,7 +147,7 @@ class LinkedList : public LinkedListInterface<T>
 		}
 		delete temp4;
 		return i;
-	}
+	};
 
 	string toString() {
 		if (head == NULL) {
@@ -175,30 +156,47 @@ class LinkedList : public LinkedListInterface<T>
 		}
 		else {
 			stringstream ss;
+      string answer;
 			Node* temp6 = head;
 			while (temp6 != NULL) {
 				if (temp6 == head) {
-					ss += temp6->data;
+					ss << temp6->data;
 					temp6 = temp6->next;
 				}
 				else {
-					ss += " ";
-					ss += temp6->data;
+					ss << " ";
+					ss << temp6->data;
 					temp6 = temp6->next;
 				}
 			}
 			delete temp6;
-			return ss;
+      ss >> answer;
+			return answer;
 		}
-	}
+	};
 
   private:
       struct Node {
         T data;
         Node* next;
         Node(const T& data_item, Node* next_ptr = NULL) : data(data_item), next(next_ptr) {}
-      }
+      };
       Node* head = NULL;
+
+      bool find(T value) {
+    		Node* temp = head;
+    		if (temp == NULL) {
+    			delete temp;
+    			return false;
+    		}
+    		while (temp->data != value) {
+    			temp = temp->next;
+    			if (temp->data == value) {
+    				delete temp;
+    				return true;
+    			}
+    		}
+    	};
 
 };
 #endif
